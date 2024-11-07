72ee20277fce56d928b90aaa44acfd72b6ebf8e1
quangpao <ddquangbao@gmail.com>
2024-11-07 09:32:03 +0700

    feat 🎉: add StudyAssist module with lifecycle methods

---
+++ index.ts
@@ -0,0 +1,18 @@
+import { BaseModule } from "4u7o";
+class StudyAssist extends BaseModule {
+  public name = "SystemModule";
+
+  protected override onLoad(): void {
+    // Do something when the module is loaded
+  }
+
+  protected override onReload(): void {
+    // Do something when the module is reloaded
+  }
+
+  protected override onUnload(): void {
+    // Do something when the module is unloaded
+  }
+}
+
+export default new StudyAssist();
